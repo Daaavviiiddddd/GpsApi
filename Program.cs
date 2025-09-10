e5ddb2b398be995a48f9679692af4b5453a789c9
Daaavviiiddddd <dv17753@gmail.com>
2025-09-10 11:41:18 -0600

    Use PORT environment variable for Render

--- Program.cs
+++ Program.cs
@@ -3,18 +3,21 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// opcional: log minimal, swagger, etc.
 builder.Services.AddControllers();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseMySql(
         builder.Configuration.GetConnectionString("DefaultConnection"),
-        new MySqlServerVersion(new Version(8, 0, 36)) // ⚠️ pon la versión de tu RDS
+        new MySqlServerVersion(new Version(8, 0, 36))
     )
 );
 
-var app = builder.Build();
+// Fuerza a Kestrel a escuchar en el puerto que Render provee
+var port = Environment.GetEnvironmentVariable("PORT") ?? "10000";
+builder.WebHost.UseUrls("http://0.0.0.0:" + port);
 
+var app = builder.Build();
 app.UseRouting();
 app.MapControllers();
-
 app.Run();
